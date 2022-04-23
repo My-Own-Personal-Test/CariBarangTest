5bb2d49530e821cbe7ce13f3da7e06b409bd6b76
M Ridwan Riansyah <67872022+ryansyrl@users.noreply.github.com>
2022-04-23 22:11:30 +0700

    Add section Questions & Testi

---
+++ components/Testi/Testi.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="section-testi w-full bg-white py-6 mb-5">
+    <div class="max-w-2xl mx-auto lg:max-w-7xl lg:px-8">
+      <div class="w-10/12 sm:w-6/12 mx-auto text-center text-gray-700 sm:py-6">
+        <h1 class="text-2xl sm:text-5xl font-bold leading-tight">
+          Mau Seperti Mereka? Daftar Sekarang
+        </h1>
+        <p
+          class="text-gray-400 font-bold italic text-base sm:text-lg text-center mt-2"
+        >
+          (Want to earn from your own international online store?<br />
+          Register Now!)
+        </p>
+      </div>
+      <div class="hidden sm:block w-full relative mt-8 mb-10">
+        <div class="bg-red-500 testy-bg"></div>
+        <div class="max-w-2xl mx-auto lg:max-w-7xl lg:px-8 z-10 relative">
+          <div class="grid grid-cols-4">
+            <div class="px-6">
+              <div
+                class="bg-white rounded-t-lg shadow-lg rounded-b-lg overflow-hidden"
+              >
+                <img
+                  src="https://caribarang.oss-ap-southeast-1.aliyuncs.com/general/lp/2.png"
+                  alt=""
+                />
+                <div class="bg-white w-full text-center py-2 flex flex-col">
+                  <span class="text-2xl text-gray-700"> 20+K USD </span>
+                  <span class="text-gray-400">per month</span>
+                </div>
+              </div>
+            </div>
+            <div class="px-6">
+              <div
+                class="bg-white rounded-t-lg shadow-lg rounded-b-lg overflow-hidden scale-16"
+              >
+                <img
+                  src="	https://caribarang.oss-ap-southeast-1.aliyuncs.com/general/lp/1.png"
+                  alt=""
+                />
+                <div class="bg-white w-full text-center py-2 flex flex-col">
+                  <span class="text-2xl text-gray-700"> 40.48K USD </span>
+                  <span class="text-gray-400">per month</span>
+                </div>
+              </div>
+            </div>
+            <div class="px-6">
+              <div
+                class="bg-white rounded-t-lg shadow-lg rounded-b-lg overflow-hidden scale-16"
+              >
+                <img
+                  src="https://caribarang.oss-ap-southeast-1.aliyuncs.com/general/lp/3.png"
+                  alt=""
+                />
+                <div class="bg-white w-full text-center py-2 flex flex-col">
+                  <span class="text-2xl text-gray-700">32.05K USD</span>
+                  <span class="text-gray-400">per month</span>
+                </div>
+              </div>
+            </div>
+            <div class="px-6">
+              <div
+                class="bg-white rounded-t-lg shadow-lg rounded-b-lg overflow-hidden"
+              >
+                <img
+                  src="https://caribarang.oss-ap-southeast-1.aliyuncs.com/general/lp/4.png"
+                  alt=""
+                />
+                <div class="bg-white w-full text-center py-2 flex flex-col">
+                  <span class="text-2xl text-gray-700">31.22+K USD</span>
+                  <span class="text-gray-400">per month</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="block sm:hidden relative">
+        <div class="bg-testi-mobile bg-red-500"></div>
+        <div class="max-w-2xl mx-auto lg:max-w-7xl py-6 px-3 z-10 relative">
+          <ul class="w-full flex flex-col">
+            <li class="mb-5">
+              <div
+                class="w-10/12 mx-auto bg-white rounded-t-lg shadow-lg rounded-b-lg overflow-hidden"
+              >
+                <img
+                  src="https://caribarang.oss-ap-southeast-1.aliyuncs.com/general/lp/1.png"
+                  alt=""
+                />
+                <div class="bg-white w-full text-center py-2 flex flex-col">
+                  <span class="text-2xl text-gray-700"> 31.22K USD </span>
+                  <span class="text-gray-400">per month</span>
+                </div>
+              </div>
+            </li>
+            <li class="mb-5">
+              <div
+                class="w-10/12 mx-auto bg-white rounded-t-lg shadow-lg rounded-b-lg overflow-hidden"
+              >
+                <img
+                  src="https://caribarang.oss-ap-southeast-1.aliyuncs.com/general/lp/2.png"
+                  alt=""
+                />
+                <div class="bg-white w-full text-center py-2 flex flex-col">
+                  <span class="text-2xl text-gray-700"> 40.48K USD </span>
+                  <span class="text-gray-400">per month</span>
+                </div>
+              </div>
+            </li>
+            <li class="mb-5">
+              <div
+                class="w-10/12 mx-auto bg-white rounded-t-lg shadow-lg rounded-b-lg overflow-hidden"
+              >
+                <img
+                  src="https://caribarang.oss-ap-southeast-1.aliyuncs.com/general/lp/3.png"
+                  alt=""
+                />
+                <div class="bg-white w-full text-center py-2 flex flex-col">
+                  <span class="text-2xl text-gray-700"> 32.05K USD </span>
+                  <span class="text-gray-400">per month</span>
+                </div>
+              </div>
+            </li>
+            <li class="mb-5">
+              <div
+                class="w-10/12 mx-auto bg-white rounded-t-lg shadow-lg rounded-b-lg overflow-hidden"
+              >
+                <img
+                  src="https://caribarang.oss-ap-southeast-1.aliyuncs.com/general/lp/4.png"
+                  alt=""
+                />
+                <div class="bg-white w-full text-center py-2 flex flex-col">
+                  <span class="text-2xl text-gray-700"> 20+K USD </span>
+                  <span class="text-gray-400">per month</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,20 @@
   <div class="w-full">
     <LandingHero />
     <Amazon />
+    <Learn />
+    <Testi />
+    <Questions />
   </div>
 </template>
 
 <script>
 import LandingHero from "~/components/Hero/LandingHero.vue";
 import Amazon from "~/components/Amazon/Amazon.vue";
+import Learn from "~/components/Learn/Learn.vue";
+import Testi from "~/components/Testi/Testi.vue";
+import Questions from "~/components/Questions/Questions.vue";
 export default {
-  components: { LandingHero, Amazon },
+  components: { LandingHero, Amazon, Learn, Testi, Questions },
   name: "IndexPage",
 };
 </script>
